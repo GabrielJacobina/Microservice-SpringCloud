4f5d8d98cca02cc85ec0b99f834d7b8cfa423f8b
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-09-08 21:33:18 -0300

    Setting up JWT authentication

---
+++ auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.youtube.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
